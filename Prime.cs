15fbd444ad91bcf581ef42c2f9d5aa90bf81f78a
ParadoxPhD <zirbrainiac@gmail.com>
2016-10-15 15:22:28 -0400

    Primes Project v1.0
    This is everything right now.
    Oct. 15 2016 - 3:22 PM EST

---
+++ Prime.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Numerics;
+using System.Diagnostics;
+
+namespace Prime
+{
+    public class Prime
+    {
+    	static Stopwatch watch = new Stopwatch();
+        static int exp = 74207281;
+        static BigInteger big = BigInteger.Pow(2, exp) - 1;
+        static TextWriter tw = new StreamWriter(@"C:\CsStuff\Prime\prime.txt");
+
+        public static void Main()
+        {
+        	watch.Start();
+            for (int i = exp; true; i++)
+            {
+                exp++;
+                primes(i);
+            }
+        }
+
+        private static void primes(int exp)
+        {
+            int div = (int)Math.Floor((double)exp/10601040);
+            int exp1 = div;
+            int exp2 = exp - (exp1 * 10601040);
+            long result = 0;
+            long j = 3;
+
+            for (long i = 3; i <= (big/j); i+=2)
+            {
+            	j+=2;
+                long result1 = (long)(Math.Pow(2, exp1) % i) * 10601040;
+                long result2 = (long)(Math.Pow(2, exp2) % i);
+                result = (((result1 * result2) - 1) % i);
+                if (result == 0)
+                {
+                    Console.WriteLine(exp + ", not prime.");
+                    return;
+                }
+                Console.WriteLine(i);
+                result = 0;
+            }
+            Console.WriteLine(exp);
+            tw.WriteLine(exp);
+        }
+    }
+}
